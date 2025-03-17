7fe626c60a374817e5be347c0a7a553a1b204084
Jam <JWalkerdine21@hotmail.com>
2025-03-17 16:11:13 +0000

    Done

--- xpce_example.pl
+++ xpce_example.pl
@@ -1,4 +1,8 @@
 
+:- use_module(library(pce)).
+
+%% https://eu.swi-prolog.org/packages/xpce/UserGuide/
+
 %% Message passing example
 %% assertz_list([]).
 %% assertz_list([H|T]) :-
@@ -55,3 +59,93 @@ graph_example :-
 % ?- graph_example.
 %@ true.
 %@ true.
+
+
+%% Built-in tools using PCE
+% ?- manpce.
+% ?- emacs.
+
+%% Fileviewer
+
+fileviewer(Dir) :-
+        new(DirObj, directory(Dir)),
+        new(F, frame('File Viewer')),
+        send(F, append(new(B, browser))),
+        send(new(D, dialog), below(B)),
+        send(D, append(button(view,
+                              message(@prolog, view,
+                                      DirObj, B?selection?key)))),
+        send(D, append(button(quit,
+                              message(F, destroy)))),
+        send(B, members(DirObj?files)),
+        send(F, open).
+
+view(DirObj, F) :-
+        send(new(V, view(F)), open),
+        get(DirObj, file(F), FileObj),
+        send(V, load(FileObj)).
+
+% ?- fileviewer(".").
+
+editing :-
+    new(Dialog, dialog("Editor Test")),
+    get(Dialog, display, Display),
+    send(Display, layout, size(400, 400)),
+    send(Dialog, append(new(_, editor))),
+    send(Dialog, open).
+
+% ?- editing.
+
+% ?- get(dialog, class, Class), get(Class, send_methods, Methods).
+
+% ?- get(dialog, get_method(layout), Get).
+
+get_method(Obj, Name, Type, Summary) :-
+    get(Obj, class, Class),
+    (get(Class, get_methods, MethodChain), Type="get"; get(Class, send_methods, MethodChain), Type="send"),
+    chain_list(MethodChain, Methods),
+    member(Method, Methods),
+    get(Method, name, Name),
+    get(Method, summary, SummaryObj),
+    get(SummaryObj, value, Summary).
+    
+    
+% ?- new(@demo, chain), get_method(@demo, Name, Type, Summary).
+% ?- new(@demo, frame), get_method(@demo, Name, Type, Summary).
+% ?- new(@demo, chain), findall(Name-Type-Summary, get_method(@demo, Name, Type, Summary), Methods).
+
+:- pce_begin_class(method_searcher, frame, "an app to search methods").
+
+variable(stored_obj, object, both, "object").
+
+initialise(F, Obj:object) :->
+    findall(Name, get_method(Obj, Name, _, _), Names),
+    send_super(F, initialise, 'Window'),
+    send(F, slot, stored_obj, Obj),
+    new(B, browser),
+    send(F, append, B),
+    send(B, members(Names)).
+
+    
+:- pce_end_class(method_searcher).
+
+% ?- new(@demo, chain), new(W, method_searcher(chain)), send(W, open).
+
+
+%% Example of keybindings
+keybinder :-
+    new(D, dialog("Layout Demo 1")),
+    %% get(D, display, Display),
+    send(D, recogniser, new(K, key_binding)),
+    send(K, function, 'a', message(@prolog, ok)),
+    send(D, open).
+
+ok :- writeln("TEST").
+
+% ?- keybinder.
+% ?- manpce.
+
+% ?- emacs.
+
+
+% ?- writeln("Ok").
