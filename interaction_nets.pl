56655e794185b8f917d0f3a340b0bf92003038d8
Jam <JWalkerdine21@hotmail.com>
2025-04-18 13:22:25 +0100

    Park this work

--- interaction_nets.pl
+++ interaction_nets.pl
@@ -50,15 +50,21 @@ compile_net(Fns, Arrows, Program) :-
     unify_slots(Fns, Arrows),
     construct_program(Fns, Program).
 
-induce(I, L, R) :- I -> L; R.
-%% induce(I, L, R) :- TermI =.. I, TermL =.. L, TermR =.. R, TermI -> TermL; R.
+induce(I, L, R) :-
+    TermI =.. I,
+    TermL =.. L,
+    TermR =.. R,
+    (TermI -> TermL; TermR).
 
 gp_add(A, B, C) :- C #= A + B.
 gp_minus(A, B, C) :- C #= A - B.
 
 gp_gensym(Fn) :- gensym('gp_fn', Fn).
 
-def_pred(LHS, RHS) :-
+def_pred(Params, Body, Fn) :-
+    gp_gensym(Fn),
+    LHS =.. [Fn|Params],
+    RHS =.. Body,
     assertz(LHS :- RHS).
 
 :- dynamic(net/3).
@@ -79,32 +85,20 @@ net('Subnet',
      [1, 4]=[4, 2]]).
 
 net('recursion',
-    [[gp_gensym, _],
-     [=.., _, [_, _, _]],
-     [compile_net, [[succ, _, _]] , [], _],
-     [compile_net, [[=, _, _]], [], _],
-     [compile_net, [[induce, _, _, _]], [], _],
-     [compile_net, [[_, _, _]], [], _],
-     [def_pred, _, _],
+    [[def_pred, [_, _], [induce, [succ, _, _], [_, _, _], [=, _, _]], _],
      [=.., _, [_, _, _]],
      [call, _],
-     [print, _]],
-    
-    [[1, 2]=[2, 3, 1],
-     [2, 2]=[5, 2, 1, 3],
-     [2, 3, 1]=[6, 2, 1, 1],
-     [2, 3, 1]=[8, 3, 1],
-     [2, 3, 3]=[6, 2, 1, 3],
-     [2, 3, 2]=[3, 2, 1, 3],
-     [2, 3, 3]=[4, 2, 1, 3],
-     [3, 2, 1, 2]=[6, 2, 1, 2],
-     [3, 2, 1, 2]=[4, 2, 1, 2],
-     [3, 4]=[5, 2, 1, 2],
-     [4, 4]=[5, 2, 1, 4],
-     [5, 4]=[7, 3],
-     [6, 4]=[7, 2],
-     [8, 2]=[9, 2],
-     [8, 3, 3]=[10, 2]]).
+     [print, _]],    
+    [[1, 2, 1]=[1, 3, 2, 2],
+     [1, 2, 1]=[1, 3, 4, 2],
+     [1, 2, 2]=[1, 3, 4, 3],
+     [1, 2, 2]=[1, 3, 3, 3],
+     [1, 3, 2, 3]=[1, 3, 3, 2],
+     [1, 3, 3, 1]=[1, 4],
+     [1, 4]=[2, 3, 1],
+     [2, 3, 3]=[4, 2],
+     [2, 2]=[3, 2]]).
+
 
 succ(1, 2).
 succ(2, 3).
@@ -117,5 +111,11 @@ succ(2, 3).
 
 % ?- compile_net('recursion', Program), Program, nl, print(Program), nl.
 %@ 3
-%@ gp_gensym(gp_fn1),gp_fn1(_10158,_10164)=..[gp_fn1,_10158,_10164],compile_net([[succ,_10200,_10158]],[],succ(_10200,_10158)),compile_net([[=,_10200,_10164]],[],_10200=_10164),compile_net([[induce,succ(_10200,_10158),gp_fn1(_10158,_10164),_10200=_10164]],[],induce(succ(_10200,_10158),gp_fn1(_10158,_10164),_10200=_10164)),compile_net([[gp_fn1,_10200,_10164]],[],gp_fn1(_10200,_10164)),def_pred(gp_fn1(_10200,_10164),induce(succ(_10200,_10158),gp_fn1(_10158,_10164),_10200=_10164)),gp_fn1(1,3)=..[gp_fn1,1,3],call(gp_fn1(1,3)),print(3)
-%@ Program = (gp_gensym(gp_fn1), gp_fn1(_A, _B)=..[gp_fn1, _A, _B], compile_net([[succ, _C, _A]], [], succ(_C, _A)), compile_net([[=, _C, _B]], [], _C=_B), compile_net([[induce, succ(..., ...)|...]], [], induce(succ(_C, _A), gp_fn1(_A, _B), _C=_B)), compile_net([[gp_fn1|...]], [], gp_fn1(_C, _B)), def_pred(gp_fn1(_C, _B), induce(succ(..., ...), gp_fn1(..., ...), ... = ...)), gp_fn1(..., ...)=..[...|...], call(...), print(...)) 
+%@ def_pred([_10128,_10134],[induce,[succ,_10128,_10170],[gp_fn1,_10170,_10134],[=,_10128,_10134]],gp_fn1),gp_fn1(1,3)=..[gp_fn1,1,3],call(gp_fn1(1,3)),print(3)
+%@ Program = (def_pred([_A, _B], [induce, [succ, _A, _C], [gp_fn1, _C, _B], [=, _A, _B]], gp_fn1), gp_fn1(1, 3)=..[gp_fn1, 1, 3], call(gp_fn1(1, 3)), print(3)) 
+
+% ?- compile_net('recursion', Program).
+
+% ?- compile_net([[def_pred, [fac, N, X], [induce, [succ, N, N1], [succ, N1, X], [=, N, X]]]], [], P), P.
+
+% ?- clause(fac(A, B), Body).
