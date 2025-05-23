e49af8aa7a612d25fc94ae920e8b97d9259c7bbd
Jam <JWalkerdine21@hotmail.com>
2025-05-23 17:00:34 +0100

    Resolve

---
+++ relational_model.pl
@@ -0,0 +1,64 @@
+
+:- dynamic rel_head/2.
+:- dynamic rel_body/2.
+:- dynamic rel_key/2.
+
+
+rel_head(s, [attribute(id, int), attribute(name, string)]).
+rel_key(s, [id]).
+
+attribute_index(RelVar, Name, Idx) :-
+    rel_head(RelVar, Attributes),
+    nth0(Idx, Attributes, attribute(Name, _)).
+
+% ?- attribute_index(s, N, I).
+
+attribute_indices(RelVar, Attributes, Indices) :-
+    maplist(attribute_index(RelVar), Attributes, Indices).
+
+rel_key_indices(RelVar, Indices) :-
+    rel_key(RelVar, Key),
+    attribute_indices(RelVar, Key, Indices).
+
+% ?- rel_key_indices(s, I).
+
+tuple_attribute(RelVar, Tuple, AttrName, AttrValue) :-
+    attribute_index(RelVar, AttrName, AttrIdx),
+    nth0(AttrIdx, Tuple, AttrValue).
+
+% ?- tuple_attribute(s, [1, "John"], id, V).
+
+tuple_attributes(RelVar, Tuple, Attrs, Vals) :-
+    maplist(tuple_attribute(RelVar, Tuple), Attrs, Vals).
+
+% ?- tuple_attributes(s, [1, "John"], [id, name], V).
+
+tuple_key(RelVar, Tuple, KeyVal) :-
+    rel_key(RelVar, Key),
+    tuple_attributes(RelVar, Tuple, Key, KeyVal).
+
+% ?- tuple_key(s, [1, "John"], V).
+
+validate_attributes([], []) :- !.
+validate_attributes([X|Xs], [attribute(_, Type)|Attributes]) :-
+    call(Type, X),
+    validate_attributes(Xs, Attributes).
+
+add_to_relation(RelVar, Tup) :-
+    rel_head(RelVar, Head),
+    rel_key_indices(RelVar, Indices),
+    
+    %% rel_key_index(RelVar, KeyIdx),
+    %% nth0(KeyIdx, Tup, KeyVal),
+    length(Head, L),
+    length(Tup, L),
+    %% not((rel_body(RelVar, Body), nth0(KeyIdx, Body, KeyVal))),
+    validate_attributes(Tup, Head),
+    assertz(rel_body(RelVar, Tup)).
+
+% ?- call(integer, 3).
+
+% ?- trace(rel_key_index/2).
+% ?- add_to_relation(s, [1, "john"]).
+
+% ?- rel_body(s, Xs).
--- structs.pl
+++ structs.pl
@@ -1,36 +1,55 @@
-:- module(structs, [make_obj/2, class/2, set_slot/3]).
+:- module(structs, [make_obj/2, class/2, set_slot/2, slot/2]).
 
 :- dynamic(object_counter/1).
 :- dynamic(class/2).
+:- dynamic(slot/2).
 
 object_counter(1).
 
 add_slots(_, _, []) :- !.
-add_slots(ObjId, Type, [(SlotName, SlotValue)|Slots]) :-
+add_slots(ObjId, Type, [SlotHead|Slots]) :-
+    SlotHead =.. [SlotName, ObjId|RestArgs],
     atom_concat(Type, '_', TypePrefix),
     atom_concat(TypePrefix, SlotName, PredName),
-    SlotTerm =.. [PredName, ObjId, SlotValue],
+    SlotTerm =.. [PredName, ObjId|RestArgs],
     asserta(SlotTerm),
+    (slot(ObjId, SlotName) -> true; asserta(slot(ObjId, SlotName))),
+    add_slots(ObjId, Type, Slots).
+add_slots(ObjId, Type, [SlotHead:-SlotBody|Slots]) :-
+    SlotHead =.. [SlotName, ObjId|RestArgs],
+    atom_concat(Type, '_', TypePrefix),
+    atom_concat(TypePrefix, SlotName, PredName),
+    SlotHeadTerm =.. [PredName, ObjId|RestArgs],
+    SlotTerm = (SlotHeadTerm:-SlotBody),
+    asserta(SlotTerm),
+    (slot(ObjId, SlotName) -> true; asserta(slot(ObjId, SlotName))),
     add_slots(ObjId, Type, Slots).
 
 make_obj(Type, Slots) :-
     object_counter(Counter),
     atom_concat(o, Counter, ObjId),
-    Existence =.. [Type, ObjId],
+    ExistencePredicate =.. [Type, ObjId],
     Counter1 is Counter + 1,
     retractall(object_counter(_)),
-    asserta(Existence),
+    asserta(ExistencePredicate),
     asserta(object_counter(Counter1)),
     asserta(class(ObjId, Type)),
     add_slots(ObjId, Type, Slots).
 
-set_slot(ID, SlotName, SlotVal) :-
-    class(ID, Class),
+set_slot(ObjId, SlotHead) :-
+    class(ObjId, Class),
+    SlotHead =.. [SlotName, ObjId|RestArgs],
     atom_concat(Class, '_', Prefix),
     atom_concat(Prefix, SlotName, SlotPredName),
-    SlotPred =.. [SlotPredName, ID, SlotVal],
-    SlotPredToRetract =.. [SlotPredName, ID, _],
-    retractall(SlotPredToRetract),
+    SlotPred =.. [SlotPredName, ObjId|RestArgs],
+    asserta(SlotPred).
+set_slot(ObjId, SlotHead:-SlotBody) :-
+    class(ObjId, Class),
+    SlotHead =.. [SlotName, ObjId|RestArgs],
+    atom_concat(Class, '_', Prefix),
+    atom_concat(Prefix, SlotName, SlotPredName),
+    SlotHeadPred =.. [SlotPredName, ObjId|RestArgs],
+    SlotPred = (SlotHeadPred:-SlotBody),
     asserta(SlotPred).
 
 get_slot(ID, SlotName, SlotVal) :-
@@ -51,16 +70,23 @@ find_obj(Type, ObjId) :-
 
 % ?- object_counter(N).
 
-% ?- make_obj(card, [(name, a)]).
-%@ true.
+% ?- make_obj(card, [name(ID, A):-A=water, type(ID, water):-true]).
 
 % ?- card(X).
+%@ Correct to: "structs:card(X)"? yes
+%@ X = o1.
 
 % ?- card_name(A, B).
+%@ Correct to: "structs:card_name(A,B)"? yes
+%@ A = o1,
+%@ B = water.
 
-% ?- set_slot(o1, type, water).
-%@ false.
+% ?- set_slot(o1, type(ID, water):-true).
+%@ ID = o1.
 
 % ?- card_type(A, B).
-% ?- X = [1,2|[1, 2]].
-%@ X = [1, 2, 1, 2].
+%@ Correct to: "structs:card_type(A,B)"? yes
+%@ A = o1,
+%@ B = water ;
+%@ A = o1,
+%@ B = water.
